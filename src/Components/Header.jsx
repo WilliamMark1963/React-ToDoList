6bc8a41802c291f1eae4c10a1f8646a350680227
LEO <kvwilliammark1963@gmail.com>
2026-04-08 12:34:04 +0530

    Last Bug Fix 08.04.2026

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,7 +2,7 @@ export function Header(){
 
     return(<>
     <h2 className="text-3xl mb-5">Appka Do List</h2>
-    <h3 className="text-2xl ">Operations that can be performed</h3>
+    <h3 className=" text-xl md:text-2xl  text-center">Operations that can be performed</h3>
     <ul className="flex my-3">
         <li className="ml-3 ">Add</li>
         <li className="ml-3 ">Delete</li>
